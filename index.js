f3e1fc24e45ed41410b147eced3dc9df09b1fb02
María <129367348+MariaPicazoSanchez@users.noreply.github.com>
2025-11-06 15:12:00 +0100

    n

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 require('dotenv').config();
 const passport=require("passport");
 // const cookieSession=require("cookie-session");
