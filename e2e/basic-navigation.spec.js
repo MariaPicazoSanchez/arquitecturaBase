42b14ec516de1af73d7f60248e4b17d4bfcc5bff
María <129367348+MariaPicazoSanchez@users.noreply.github.com>
2026-01-17 09:19:51 +0100

    pruebas con problemas

--- e2e/basic-navigation.spec.js
+++ e2e/basic-navigation.spec.js
@@ -1,41 +1,34 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('basic navigation', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-
-  // Iniciar sesión
   await page.locator('#fmRegistro').getByRole('button', { name: 'Iniciar sesión' }).click();
   await page.getByRole('textbox', { name: 'Email' }).click();
   await page.getByRole('textbox', { name: 'Email' }).fill('maria.picazo5@alu.uclm.es');
   await page.getByRole('textbox', { name: 'Contraseña' }).click();
   await page.getByRole('textbox', { name: 'Contraseña' }).fill('Hola1234');
   await page.locator('#btnLogin').click();
-
-  // Navegar a la cuenta de usuario
   await page.getByRole('button', { name: 'Usuario' }).click();
   await page.getByRole('link', { name: 'Mi cuenta' }).click();
-
-  // Actualizar información de la cuenta
   await page.getByRole('textbox', { name: 'Nombre' }).click();
   await page.getByRole('textbox', { name: 'Nombre' }).fill('UsuarioPrueba');
   await page.getByRole('textbox', { name: 'Nick' }).click();
   await page.getByRole('textbox', { name: 'Nick' }).fill('UserPrueba');
   await page.getByRole('button', { name: 'Guardar cambios' }).click();
   await page.getByRole('button', { name: 'Volver' }).click();
-
-  // Jugar una partida
   await page.getByRole('button', { name: 'Entrar' }).first().click();
   await page.getByRole('button', { name: 'Crear partida' }).click();
   await page.getByLabel('Modo').selectOption('PVBOT');
   await page.getByRole('button', { name: 'Crear', exact: true }).click();
   await page.getByRole('button', { name: 'Salir al lobby' }).click();
-
-  // Repetir el proceso de jugar y abandonar partidas
-  for (let i = 0; i < 2; i++) {
-    await page.getByRole('button', { name: 'Entrar' }).nth(i + 1).click();
-    await page.getByRole('button', { name: 'Crear partida' }).click();
-    await page.getByLabel('Modo').selectOption('PVBOT');
-    await page.getByRole('button', { name: 'Crear', exact: true }).click();
-    await page.getByRole('button', { name: 'Abandonar' }).click();
-  }
+  await page.getByRole('button', { name: 'Entrar' }).nth(1).click();
+  await page.getByRole('button', { name: 'Crear partida' }).click();
+  await page.getByLabel('Modo').selectOption('PVBOT');
+  await page.getByRole('button', { name: 'Crear', exact: true }).click();
+  await page.getByRole('button', { name: 'Abandonar' }).click();
+  await page.getByRole('button', { name: 'Entrar' }).nth(2).click();
+  await page.getByRole('button', { name: 'Crear partida' }).click();
+  await page.getByLabel('Modo').selectOption('PVBOT');
+  await page.getByRole('button', { name: 'Crear', exact: true }).click();
+  await page.getByRole('button', { name: 'Abandonar' }).click();
 });
